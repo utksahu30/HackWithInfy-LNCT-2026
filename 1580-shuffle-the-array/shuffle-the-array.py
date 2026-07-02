2c4a4ce60c8d67e3751b0c0b86dab46841372444
Utkarsh Sahu <142162415+utksahu30@users.noreply.github.com>
2026-07-02 12:21:29 +0530

    Time: 68 ms (29.04%) | Memory: 19.4 MB (16.43%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,11 +1,12 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        n = len(nums)
-        num1 = nums[0:n//2]
-        num2 = nums[n//2:n]
-        i = 0
+        #n = len(nums)
+        #n already defined and if n is even, no need to use //2 all the time
+        num1 = nums[0:n]
+        num2 = nums[n:]
+        #i = 0
         temp = []
-        for i in range(0,n//2):
+        for i in range(0,n):
             temp.append(num1[i])
             temp.append(num2[i])
             
